9e496a9f6c88921767ff3acb7821e6d29c74b170
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-03-10 21:04:10 +0900

    like機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
-  
+
   accepts_nested_attributes_for :photos
+
+  def liked_by(user)
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end

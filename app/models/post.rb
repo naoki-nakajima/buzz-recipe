da33b70ada07fed1cf21a652910e482f58a3ba49
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-03-10 21:00:28 +0900

    　like-model アソシエーション　バリデーションの実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
-
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+  
   accepts_nested_attributes_for :photos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_133735) do
+ActiveRecord::Schema.define(version: 2021_03_10_115630) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
@@ -42,6 +51,8 @@ ActiveRecord::Schema.define(version: 2021_03_09_133735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end

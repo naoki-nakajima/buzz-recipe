31241becf3b176a586c69e5c5a1a38c69ac828d7
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-04-02 22:21:41 +0900

    link_to_remove_associationの編集

--- db/migrate/20210309133636_create_posts.rb
+++ db/migrate/20210309133636_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.string :title
       t.string :caption
+      t.string :how_to_cock
+      t.string :post_comment
       t.references :user, foreign_key: true, null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,10 @@ ActiveRecord::Schema.define(version: 2021_03_27_115619) do
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
     t.string "caption"
+    t.string "how_to_cock"
+    t.string "post_comment"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

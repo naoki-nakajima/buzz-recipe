6458ff5cb6729e77b884547306e2e13419844b2a
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-05-10 14:34:51 +0900

    アコーディオンメニューの実装

--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -19,21 +19,34 @@ $(document).on('turbolinks:load', function () {
   tabs.on('click', tabChange);
 });
 
-$(function () {
-  $(".more_shop_info").on("click", function () {
-    $(this).toggleClass("on-click");
-    $(".info_hide").slideToggle(500);
+
+$(document).on('turbolinks:load', function () {
+  $(".more_shop_info").on('click', function () {
+    var show_text = $(".info_item");
+    var small_height = 40; //This is initial height.
+    var original_height = show_text.css({ height: "auto" }).height();
+
+    if (show_text.hasClass("open")) {
+      show_text.height(original_height).animate({ height: small_height }, 300);
+      show_text.removeClass("open");
+      $(this).text("+ 続きを読む");
+    } else {
+      show_text
+        .height(small_height).animate({ height: original_height }, 300, function () {
+          show_text.height("auto");
+        });
+      show_text.addClass("open");
+      $(this).text("- 閉じる");
+    }
   });
 });
 
-$(function () {
+
+$(document).on('turbolinks:load', function () {
   $(".more_hours").on("click", function () {
     $(this).toggleClass("on-click");
     $(".shop_hour").slideToggle(500);
   });
 });
-
-
-
 
 

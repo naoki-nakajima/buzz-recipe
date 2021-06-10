22dca9dc5c09eed37959a6327ad42fd35240e2ca
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-06-10 22:00:40 +0900

    ショップ管理者用のdeviseの設定、ルーティング、コントローラー、サインアップのビューまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :shop_admins, controllers: {
+    sessions: 'shop_admins/sessions',
+    password: 'shop_admins/passwords',
+    registrations: 'shop_admins/registrations'
+  }
   devise_for :users, controllers: { 
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_login'
   end
   
   root "posts#index"
-  
-  
-  get '/photo/hashtag/:name', to: 'posts#hashtag'
 
   resources :posts do
     resources :photos, only: %i(create)
@@ -20,6 +23,9 @@ Rails.application.routes.draw do
       get :search
     end
   end
+  
   resources :users, only: %i(index show edit)
+  
+  get '/photo/hashtag/:name', to: 'posts#hashtag'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_134317) do
+ActiveRecord::Schema.define(version: 2021_06_10_125010) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -64,6 +64,18 @@ ActiveRecord::Schema.define(version: 2021_06_09_134317) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["caption"], name: "index_posts_on_caption", length: 32
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "shop_admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_shop_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_shop_admins_on_reset_password_token", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

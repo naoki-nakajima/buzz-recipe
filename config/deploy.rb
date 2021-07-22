d003c7543f25c30f3f6543497f6093c5ff2bebb9
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-07-22 17:32:33 +0900

    /var/lib/mysql/mysql.sockに変更

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.16.0'
+
+set :application, 'buzz-recipe'
+
+set :repo_url,  'git@github.com:naoki-nakajima/buzz-recipe.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.7.2'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/nakajimanaoki.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
